e3ffe5cef5d858c6a603885a83963f308bdd5a33
Agustin <alarreine@gmail.com>
2017-04-12 08:53:13 +0200

    Firs Test

---
+++ src/test/java/Test.java
@@ -0,0 +1,19 @@
+import junit.framework.TestCase;
+
+import static org.junit.Assert.assertEquals;
+import junit.framework.*;
+
+/**
+ * Created by alarreine on 12/04/2017.
+ */
+
+public class Test extends TestCase {
+
+
+
+    public void testCalculer() throws Exception {
+
+        assertEquals(1,1);
+    }
+
+}
