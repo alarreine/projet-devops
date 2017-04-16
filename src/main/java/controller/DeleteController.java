8e2de2a18ea6d3c9a36239643953b4f643817ba3
minchunnguyen <minchun_nguyen@yahoo.com>
2017-04-16 23:32:16 +0200

    Controlleur PUT_DELETE

--- src/main/java/controller/DeleteController.java
+++ src/main/java/controller/DeleteController.java
@@ -1,8 +1,15 @@
 package controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import application.Application;
+import bean.Client;
+import bean.reponse.Basic;
+import bean.reponse.Information;
+import bean.requete.Delete;
+import com.google.gson.Gson;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by alarreine on 15/04/2017.
@@ -10,4 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(method = RequestMethod.DELETE)
 public class DeleteController {
+    @RequestMapping(value = "/{client}/delete/{k}")
+    @ResponseStatus(HttpStatus.OK)
+
+    public Basic deleteKey(@PathVariable String client, @RequestBody Delete d, HttpServletRequest request){
+        Client cli = new Client(client,request.getRemoteAddr());
+        String result = Application.getServer().effacerInformation(cli, d.getKey());
+        Gson gson = new Gson();
+        Information reponseInformation = gson.fromJson(result,Information.class);
+        return  reponseInformation;
+    }
 }
--- src/main/java/controller/PutController.java
+++ src/main/java/controller/PutController.java
@@ -1,12 +1,17 @@
 package controller;
 
+import application.Application;
+import bean.Client;
 import bean.reponse.Basic;
+import bean.reponse.Information;
+import bean.requete.Increase;
 import bean.requete.RenameKey;
-import controller.exception.KeyNotFoundException;
-import controller.exception.PostConflicException;
-import enumerate.StatusReponse;
+import bean.requete.SetInformation;
+import com.google.gson.Gson;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Controlleur pour les méthodes qui font de mise-a-jour des informations stockées
@@ -15,15 +20,37 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(method = RequestMethod.PUT)
 public class PutController {
 
-    @RequestMapping(value = "/rename")
+    @RequestMapping(value = "/{client}/rename/{renameKey}")
     @ResponseStatus(HttpStatus.OK)
-    public Basic renomeCle(@RequestBody RenameKey renomeCles) {
 
-        Basic reponseBasic = new Basic();
-        if(renomeCles.getKey()=="1") {
-            throw new PostConflicException(renomeCles.getKey());
-        }
-        reponseBasic.setStatus(StatusReponse.OK);
-        return reponseBasic;
+    public Basic renameKey(@PathVariable String client, @RequestBody RenameKey renomeCles,HttpServletRequest request) {
+        Client cli = new Client(client,request.getRemoteAddr());
+        String result = Application.getServer().renomeCle(cli,renomeCles.getKey(),renomeCles.getNewKey());
+        Gson gson = new Gson();
+        Information reponseInformation = gson.fromJson(result,Information.class);
+        return  reponseInformation;
     }
+
+    @RequestMapping(value = "/{client}/increase/{increase}")
+    @ResponseStatus(HttpStatus.OK)
+
+    public Basic increaseKey(@PathVariable String client, @RequestBody Increase iK, HttpServletRequest request){
+        Client cli = new Client(client,request.getRemoteAddr());
+        String result = Application.getServer().incrementerInformation(cli, iK.getKey());
+        Gson gson = new Gson();
+        Information reponseInformation = gson.fromJson(result,Information.class);
+        return  reponseInformation;
+    }
+
+
+    @RequestMapping(value = "/{client}/addlist/{setInformation}")
+    @ResponseStatus(HttpStatus.OK)
+    public Basic addListe(@PathVariable String client, @RequestBody SetInformation sI, HttpServletRequest request){
+        Client cli = new Client(client,request.getRemoteAddr());
+        String result = Application.getServer().setInformation(cli, sI.getKey(),sI.getListInfo().getInfo().);
+        Gson gson = new Gson();
+        Information reponseInformation = gson.fromJson(result,Information.class);
+        return  reponseInformation;
+    }
+
 }
