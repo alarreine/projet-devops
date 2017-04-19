b684fc03c7a5056a2ba61c21767243ad0c6d386e
Julien Dides <julien.dides@gmail.com>
2017-04-19 11:23:51 +0200

    testes

--- src/test/java/TestGetSet.java
+++ src/test/java/TestGetSet.java
@@ -1,4 +1,5 @@
 import bean.reponse.ReponseBasic;
+import bean.requete.RequeteSetInformation;
 import enumerate.StatusReponse;
 import junit.framework.TestCase;
 import controller.*;
@@ -7,11 +8,17 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Arrays;
 
@@ -25,28 +32,56 @@ import java.util.Arrays;
 /**
  * Created by alarreine on 12/04/2017.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemanderControleur.class)
+@WebAppConfiguration
+public class TestGetSet extends TestCase{
 
 
-	/*@Test
-	public void testSetCreation(){
+    private MockMvc mockMvc;
 
-		assertTrue(this.stocker.stockerInformationParCle("cle", "contenu de 'cle'").getStatusCode().is2xxSuccessful());
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void testGetCleInexistante() throws Exception{
+        String cle="cle";
+        this.mockMvc.perform(get("/key/{id}", cle))
+                .andExpect(status().isNotFound());
+    }
+
+
+	@Test
+	public void testSetCreation() throws Exception{
+        String cle = "cle";
+		String valeur = "contenu de 'cle'";
+        RequeteSetInformation info = new RequeteSetInformation();
+        info.setKey(cle);
+        info.setInfo(valeur);
+		this.mockMvc.perform(put("/info/{id}",info))
+                .andExpect(status().isAccepted());
     }
 
 	@Test
-	public void testGetCleExistant(){
-		String cle = "cle";
-		String valeur = "contenu de 'cle'";
-		assertTrue(this.stocker.stockerInformationParCle(cle, valeur).getStatusCode().is2xxSuccessful());
-		assertEquals(this.demander.obtenirInformationParCle(cle).getInformation(), valeur);
-	}
+	public void testGetCleExistant() throws Exception{
 
-	@Test
-	public void testGetCleInexistante(){
-		String cle = "cle";
-		String valeur = "contenu de 'cle'";
-		assertNotEquals(this.demander.obtenirInformationParCle(cle).getInformation(), valeur);
-		assertFalse(this.demander.obtenirInformationParCle(cle).exist());
+        String cle = "cle";
+        String valeur = "contenu de 'cle'";
+        RequeteSetInformation info = new RequeteSetInformation();
+        info.setKey(cle);
+        info.setInfo(valeur);
+        this.mockMvc.perform(put("/info/{info}",info))
+                .andExpect(status().isAccepted());
+        this.mockMvc.perform(get("/key/{id}",cle))
+                .andExpect(status().isFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$.key", is(1)));
+        verify(valeur);
 	}
 
 	@Test
@@ -54,17 +89,37 @@ import java.util.Arrays;
 		String cle = "cle";
 		String valeur1 = "contenu de 'cle'";
 		String valeur2 = "nouveau contenu de 'cle'";
-		assertTrue(this.stocker.stockerInformationParCle(cle, valeur1).getStatusCode().is2xxSuccessful());
-		assertTrue(this.stocker.stockerInformationParCle(cle, valeur2).getStatusCode().is2xxSuccessful());
-		assertEquals(this.demander.obtenirInformationParCle(cle).getInformation(), valeur2);
+        RequeteSetInformation info1 = new RequeteSetInformation();
+        RequeteSetInformation info2 = new RequeteSetInformation();
+        info1.setKey(cle);
+        info2.setKey(cle);
+        info1.setInfo(valeur1);
+        info2.setInfo(valeur2);
+        this.mockMvc.perform(put("/info/{id}",info1))
+                .andExpect(status().isAccepted());
+        this.mockMvc.perform(put("/info/{id}",info2))
+                .andExpect(status().isAccepted());
+        this.mockMvc.perform(get("/key/{id}",cle))
+                .andExpect(status().isFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$.key", is(1)));
+        verify(valeur2);
 	}
 
 	@Test
 	public void testSetCaractereSpeciaux(){
 		String cle = "clé 100% spécial !§*$£^ù c'est comme ça ;) \"voilà voilà\"";
 		String valeur = "Le contenu est pas mal non plus \\bonjour\\/\\";
-		assertTrue(this.stocker.stockerInformationParCle(cle, valeur).getStatusCode().is2xxSuccessful());
-		assertEquals(this.demander.obtenirInformationParCle(cle).getInformation(), valeur);
+        RequeteSetInformation info = new RequeteSetInformation();
+        info.setKey(cle);
+        info.setInfo(valeur);
+        this.mockMvc.perform(put("/info/{id}",info))
+                .andExpect(status().isAccepted());
+        this.mockMvc.perform(get("/key/{id}",cle))
+                .andExpect(status().isFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$.key", is(1)));
+        verify(valeur);
 	}
-*/
-//}
+
+}
